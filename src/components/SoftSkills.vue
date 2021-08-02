6f6ae327450c4b02aacd83b731b7abe93fa245b4
softechsuperstar <perv.expert610@outlook.com>
2021-08-01 17:00:18 -0700

    complete softskills

---
+++ src/components/SoftSkills.vue
@@ -0,0 +1,67 @@
+<template>
+  <div 
+    class="border-8 divide-solid divide-gray-300 p-4"
+  >
+    <p class="text-2xl">Soft Skills</p>
+    <div 
+      v-for="(softskill, index) in softskills" :key="index"
+      class="mb-3 flex justify-between items-center pr-8"
+    >
+      <div  
+        class="bg-yellow-400 p-1 rounded"
+      >
+        {{ softskill.text }}
+      </div>
+      <div
+        class="flex justify-end"
+      >
+          <div 
+            v-for="(n, numindex) in softskill.number" :key="numindex + '_index'"
+            class="w-4 h-4 bg-blue-700 rounded-full mr-2"
+          ></div>
+          <div 
+            v-for="(n, numindex) in 5 - softskill.number" :key="numindex + 'index'"
+            class="w-4 h-4 bg-blue-300 rounded-full mr-2"
+          ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  watch: {
+  },
+  name: 'SoftSkills',  
+  data: () => ({
+    softskills: [
+      {
+        text: 'Lidership',
+        number: 3
+      },
+      {
+        text: 'Empath',
+        number: 4
+      },
+      {
+        text: 'Comunication',
+        number: 3
+      },
+      {
+        text: 'criativity',
+        number: 5
+      },
+    ]
+  }),
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+  created() {
+    
+  },
+}
+</script>
